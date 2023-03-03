e1692459cc56f3d98a2d56d2caf2ecf92d0f110b
Sergey Gorsky <gorsky@mail.ru>
2023-03-03 18:59:35 +0300

    07. Быстрая, слиянием и внешняя сортировки (метод generate)

--- src/main/java/ru/algo/common/CommonExternalSort.java
+++ src/main/java/ru/algo/common/CommonExternalSort.java
@@ -1,9 +1,30 @@
 package ru.algo.common;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public abstract class CommonExternalSort {
 
     protected static final int PART_SIZE = 1000;
     protected static final String INPUT_FILE_NAME = "/opt/payload.dat";
     protected static final String OUTPUT_FILE_NAME = "/opt/payload_sorted.dat";
     protected static final String PARTS_DIR = "/opt/sort";
+
+    protected void generate() {
+        final int n = 10000;
+        final int t = 100000;
+
+        var random = new Random(t);
+
+        try (FileWriter writer = new FileWriter(INPUT_FILE_NAME)) {
+            for (int i = 1; i <= n; i++) {
+                int num = random.nextInt(t);
+                writer.write(num + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
